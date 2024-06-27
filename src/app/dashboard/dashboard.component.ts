7ecf7392c9d3669b4d76fb54da6b4a6bd2cf3e7a
philus20 <theophilus.quaicoe1@icloud.com>
2024-06-27 18:17:06 +0000

    third commit at the library

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,11 +6,15 @@ import { ScheduleComponent } from '../schedule/schedule.component';
 import { ExamsTimeTableComponent } from '../exams-time-table/exams-time-table.component';
 import { ResultsComponent } from '../results/results.component';
 import { CoursesComponent } from '../courses/courses.component';
+import { AnnouncementComponent } from '../announcement/announcement.component';
+import { AccountSettingsComponent } from '../account-settings/account-settings.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule, DatePipe,CommonModule,ScheduleComponent,ExamsTimeTableComponent,ResultsComponent,CoursesComponent], // Add DatePipe to the imports array
+  imports: [RouterModule, DatePipe,CommonModule,ScheduleComponent,ExamsTimeTableComponent,ResultsComponent,CoursesComponent,
+    AnnouncementComponent, AccountSettingsComponent
+  ], // Add DatePipe to the imports array
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   providers: [DatePipe] // No need to add Router to providers array
@@ -23,6 +27,8 @@ export class DashboardComponent implements OnInit {
   exams_time_table :boolean = false;
   show_results : boolean = false;
   show_courses : boolean = false;
+  show_announcements : boolean = false;
+  show_accountsettings : boolean = false;
 
   constructor(private datePipe: DatePipe, private router: Router) { }
 
@@ -37,6 +43,9 @@ export class DashboardComponent implements OnInit {
     this.exams_time_table=false;
     this.show_results = false;
     this.show_courses = false;
+    this.show_announcements = false;
+    this.show_accountsettings = false;
+
   }
   showSchedule(){
     this.dashboard_content = false;
@@ -44,6 +53,9 @@ export class DashboardComponent implements OnInit {
     this.exams_time_table=false;
     this.show_results = false;
     this.show_courses = false;
+    this.show_announcements = false;
+    this.show_accountsettings = false;
+
   }
 showExamsTimeTable(){
   this.dashboard_content = false;
@@ -51,6 +63,9 @@ showExamsTimeTable(){
   this.show_results = false;
   this.exams_time_table = true;
   this.show_courses = false;
+  this.show_announcements = false;
+  this.show_accountsettings = false;
+
 }
 
 showResults(){
@@ -59,6 +74,9 @@ showResults(){
   this.exams_time_table = false;
   this.show_results = true;
   this.show_courses = false;
+  this.show_announcements = false;
+  this.show_accountsettings = false;
+
 }
 showCourses(){
   this.dashboard_content = false;
@@ -66,6 +84,29 @@ showCourses(){
   this.exams_time_table = false;
   this.show_results = false;
   this.show_courses = true;
+  this.show_announcements = false;
+  this.show_accountsettings = false;
+
+}
+showAnnouncements(){
+  this.dashboard_content = false;
+  this.schedule_content = false;
+  this.exams_time_table = false;
+  this.show_results = false;
+  this.show_courses = false;
+  this.show_accountsettings = false;
+  this.show_announcements = true;
+
+}
+
+showAccount(){
+  this.dashboard_content = false;
+  this.schedule_content = false;
+  this.exams_time_table = false;
+  this.show_results = false;
+  this.show_courses = false;
+  this.show_announcements = false;
+  this.show_accountsettings = true;
 }
 
   goToSchedule() {
